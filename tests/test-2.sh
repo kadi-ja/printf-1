47a0eef2d79d4ba35e81242320c47c57308d4e71
“01010110-Code” <tantaouibahaabusiness@gmail.com>
2023-03-25 15:53:05 -0700

    remove dthe additional testing files

--- tests/test-2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if gcc -Wall -Wextra -Werror -pedantic -std=gnu89 -Wno-format *.c tests/main-2.c -o printf; then
-    echo "Your output: "
-    ./printf
-else
-    echo "Compilation failed"
-fi

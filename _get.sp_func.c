cc257d4f5deabe2013b03c9cf6cbeb0dbeec4594
ZinoChan <devzinochan@gmail.com>
2023-03-26 21:08:32 +0000

    sorry but I had to refactor to keep my sanity XD

---
+++ _get.sp_func.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+int (*get_sp_func(char format))(va_list)
+{
+	int i;
+		print_t sp[] = {
+		{'c', _print_a_char},
+		{'s', _print_a_string},
+		{'u', print_uns_int},
+		{'o', print_octal},
+		{'x', print_uns_hex},
+		{'X', print_uns_HEX},
+		{'S', print_S},
+		{'p', print_p},
+		{'\0', NULL}
+	};
+	
+
+	for (i = 0; sp[i]._type; i++)
+	{
+		if (format == sp[i]._type)
+		{
+			return (sp[i].f);
+		}
+	}
+	return (NULL);
+}
--- main.h
+++ main.h
@@ -12,20 +12,18 @@
   * @specifier: The conversion specifier
   * @f: The function pointer
   */
-typedef struct specifiers
+typedef struct sp
 {
-	char *specifier;
+	char _type;
 	int (*f)(va_list args);
-} spc_dt;
+} print_t;
 
+int (*get_sp_func(char format))(va_list);
 int _write(char c);
 int _printf(const char *format, ...);
 int _print_a_char(va_list args);
 int _print_a_string(va_list args);
 int _print_format(const char *format, va_list args);
-int _print_spec(char format, va_list args);
-int _print_invalid_spec(char prev_format, char format, int count);
-int _validate_char(char _type);
 int print_uns_int(va_list args);
 int print_octal(va_list args);
 int print_uns_hex(va_list args);

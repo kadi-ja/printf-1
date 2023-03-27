a7c5b06141fe42d6a5e573ad7d76360927d36b41
“01010110-Code” <tantaouibahaabusiness@gmail.com>
2023-03-26 20:17:01 -0700

    added the handler of %i and %d to a new file called printers_05.c

--- printers_05.c
+++ printers_05.c
@@ -16,3 +16,22 @@ int d_handler(va_list list)
 	}
 	return (0);
 }
+
+
+/**
+*i_handler - A function to print a unsigned integer
+*@list: variadic argument list string to print
+*Return: length of the printed string
+*/
+
+int i_handler(va_list list)
+{
+	int l;
+
+	if (list)
+	{
+		l = _write(va_arg(list, int));
+		return (l);
+	}
+	return (0);
+}

b17ec3ea4047e5da4046b03a54dd31648ca478a4
ZinoChan <devzinochan@gmail.com>
2023-03-28 18:42:42 +0000

    remove repetitive code

--- version_01/printers_04.c
+++ version_01/printers_04.c
@@ -39,52 +39,13 @@ int print_S(va_list args)
 
 
 /**
-* print_X - print number to hexa upper
-* @x: number to print
-* Return: number of bytes printed
-**/
-int print_X(unsigned long int x)
-{
-	int res, i, j, count = 0;
-	char *ptr;
-	unsigned int  y = x;
-
-	if (x < 1)
-	{
-		_write('0' + 0);
-		return (1);
-	}
-	for (j = 0; y > 0; j++)
-		y = y / 16;
-	ptr = malloc(sizeof(char) * j);
-	if (ptr == NULL)
-		return (-1);
-	for (i = 0; x > 0; i++)
-	{
-		res = x % 16;
-		x = x / 16;
-		if (res > 9 && res < 16)
-			ptr[i] = 7 + res;
-		else
-			ptr[i] = res;
-		count++;
-	}
-	for (; i > 0; i--)
-	{
-		_write('0' + ptr[i - 1]);
-	}
-	free(ptr);
-	return (count);
-}
-
-/**
 * print_p - print pointer address
 * @args: pointer arguments
 * Return: number of bytes printed
 **/
 int print_p(va_list args)
 {
-	unsigned long n;
+	unsigned long int n;
 	int count = 0;
 
 	n = va_arg(args, unsigned long int);
@@ -97,46 +58,9 @@ int print_p(va_list args)
 
 	_write('0');
 	_write('x');
-	count = count + print_x(n) + 2;
+	count += print_unsigned_hex(n, 0) + 2;
 	return (count);
 }
 
 
-/**
-* print_x - print number to hexa lower
-* @x: number to print
-* Return: number of bytes printed
-**/
-int print_x(unsigned long int x)
-{
-	int res, i, j, count = 0;
-	char *ptr;
-	unsigned int  y = x;
 
-	if (x < 1)
-	{
-		_write('0' + 0);
-		return (1);
-	}
-	for (j = 0; y > 0; j++)
-		y = y / 16;
-	ptr = malloc(sizeof(char) * j);
-	if (ptr == NULL)
-		return (-1);
-	for (i = 0; x > 0; i++)
-	{
-		res = x % 16;
-		x = x / 16;
-		if (res > 9 && res < 16)
-			ptr[i] = 39 + res;
-		else
-			ptr[i] = res;
-		count++;
-	}
-	for (; i > 0; i--)
-	{
-		_write('0' + ptr[i - 1]);
-	}
-	free(ptr);
-	return (count);
-}

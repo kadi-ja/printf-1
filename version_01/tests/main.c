f45e6a77bbfe005cefb0249e1ee810de390a07fd
ZinoChan <devzinochan@gmail.com>
2023-03-28 10:28:43 +0000

    add more tests

--- version_01/tests/main.c
+++ version_01/tests/main.c
@@ -11,9 +11,18 @@ int main(void)
 {
     unsigned int ui;
     void *addr;
+     int len;
+    int len2;
+      len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
 
     addr = (void *)0x7ffe637541f0;
     ui = (unsigned int)INT_MAX + 1024;
+        _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
+
    _printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
@@ -28,7 +37,6 @@ int main(void)
     printf("Address:[%p]\n", addr);
      _printf("%S\n", "Best\nSchool");
 
-    printf("%d, %d\n",  _printf("Character:[%c]\n", 'H'), printf("Character:[%c]\n", 'H'));
-    printf("%d, %d\n",   _printf("String:[%s]\n", "I am a string !"), printf("String:[%s]\n", "I am a string !"));
+
     return (0);
 }
